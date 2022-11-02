8429b8f342041311d5b7a2344cb8cc904772f611
DOLGIY-DMITRIY <dolgiy.dmitriyp@gmail.com>
2022-11-02 18:05:09 +0100

    Added clipboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,27 @@
 let input = document.getElementById("url")
 let cut = document.getElementById("cut")
 let setup = document.getElementById("set-up")
+let copy = document.getElementById("clipboard")
+let text = document.getElementById("copy-text")
+
+
+copy.addEventListener("click", function (e) {
+    try {
+        navigator.clipboard.writeText(text.innerHTML);
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+
+    copy.style.border = "2px solid rgb(29, 207, 41)"
+    text.style.color = "rgb(29, 207, 41)"
+
+
+    setTimeout(function () {
+        copy.style.border = "2px solid white"
+        text.style.color = "white"
+    }, 3000)
+})
+
 
 cut.addEventListener("click", function (e) {
     const xhr = new XMLHttpRequest();
@@ -13,7 +34,7 @@ cut.addEventListener("click", function (e) {
     xhr.onreadystatechange = () => { // Call a function when the state changes.
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             res = xhr.responseText.substring(xhr.responseText.indexOf("</html>") + "</html>".length)
-            console.log(res)
+            text.innerHTML = "localhost:8081/" + res
         }
     }
     xhr.send(data);
